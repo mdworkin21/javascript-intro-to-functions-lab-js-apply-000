608fad15ea9558f0d672cd8883012de3968518dd
Matthew Dworkin <mdworkin21@gmail.com>
2018-02-23 23:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === true) {
-    return "I can't hear you!";
+    return "I can\'t hear you!";
   } else if (string.toUpperCase() === true) {
     return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
